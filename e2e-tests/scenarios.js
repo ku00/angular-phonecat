8a39dfea5cba571c1b43d75ed0a7bacbd232b9c7
ku00 <skentaro36@gmail.com>
2016-07-14 19:01:13 +0900

    wip step-9
    TODO: fix e2e test

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -7,53 +7,76 @@ describe('PhoneCat Application', function() {
       browser.get('index.html');
     });
 
-    it('should filter the phone list as a user types into the search box', function() {
-      var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
-      var query = element(by.model('$ctrl.query'));
-
-      expect(phoneList.count()).toBe(20);
-
-      query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
-
-      query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(8);
+    it('should redirect `index.html` to `index.html#!/phones', function() {
+      browser.get('index.html');
+      expect(browser.getLocationAbsUrl()).toBe('/phones');
     });
 
-    it('should be possible to control phone order via the drop-down menu', function() {
-      var queryField = element(by.model('$ctrl.query'));
-      var orderSelect = element(by.model('$ctrl.orderProp'));
-      var nameOption = orderSelect.element(by.css('option[value="name"]'));
-      var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+    describe('View: Phone list', function() {
+      beforeEach(function() {
+        browser.get('index.html#!/phones');
+      });
 
-      function getNames() {
-        return phoneNameColumn.map(function(elem) {
-          return elem.getText();
-        });
-      }
+      it('should filter the phone list as a user types into the search box', function() {
+        var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+        var query = element(by.model('$ctrl.query'));
 
-      queryField.sendKeys('tablet');
+        expect(phoneList.count()).toBe(20);
 
-      expect(getNames()).toEqual([
-        'Motorola XOOM\u2122 with Wi-Fi',
-        'MOTOROLA XOOM\u2122'
-      ]);
+        query.sendKeys('nexus');
+        expect(phoneList.count()).toBe(1);
 
-      nameOption.click();
+        query.clear();
+        query.sendKeys('motorola');
+        expect(phoneList.count()).toBe(8);
+      });
 
-      expect(getNames()).toEqual([
-        'MOTOROLA XOOM\u2122',
-        'Motorola XOOM\u2122 with Wi-Fi'
-      ]);
+      it('should be possible to control phone order via the drop-down menu', function() {
+        var queryField = element(by.model('$ctrl.query'));
+        var orderSelect = element(by.model('$ctrl.orderProp'));
+        var nameOption = orderSelect.element(by.css('option[value="name"]'));
+        var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+
+        function getNames() {
+          return phoneNameColumn.map(function(elem) {
+            return elem.getText();
+          });
+        }
+
+        queryField.sendKeys('tablet');
+
+        expect(getNames()).toEqual([
+          'Motorola XOOM\u2122 with Wi-Fi',
+          'MOTOROLA XOOM\u2122'
+        ]);
+
+        nameOption.click();
+
+        expect(getNames()).toEqual([
+          'MOTOROLA XOOM\u2122',
+          'Motorola XOOM\u2122 with Wi-Fi'
+        ]);
+      });
+
+      it('should render phone specific links', function() {
+        var query = element(by.model('$ctrl.query'));
+        query.sendKeys('nexus');
+
+        element.all(by.css('.phones li a')).first().click();
+        expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
+      });
+
     });
 
-    it('should render phone specific links', function() {
-      var query = element(by.model('$ctrl.query'));
-      query.sendKeys('nexus');
+    describe('View: Phone details', function() {
+      beforeEach(function() {
+        browser.get('index.html#!/phones/nexus-s');
+      });
 
-      element.all(by.css('.phones li a')).first().click();
-      expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
+      it('should display placeholder page with `phoneId`', function() {
+        expect(element(by.binding('$ctrl.phoneId')).getText()).toBe('nexus-s');
+      });
+
     });
 
   });
