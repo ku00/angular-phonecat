d3b62e442e52f015e12f7086cf48785f78b83f39
ku00 <skentaro36@gmail.com>
2017-12-17 23:56:27 +0900

    minify js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   entry: './app/app',
   output: {
     path: path.resolve(__dirname, './app/dist'),
     filename: 'bundle.js'
-  }
+  },
+
+  devtool: 'source-map',
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    })
+  ]
 };
 
 module.exports = config;
